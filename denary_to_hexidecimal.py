6d07511ae326a8656cf09df4fa8df37978ed5cea
Ischlosser <205675049+Ischlosser@users.noreply.github.com>
2025-09-21 11:56:06 +0200

    added hex converters (not finished)

---
+++ denary_to_hexidecimal.py
@@ -0,0 +1,4 @@
+def denarytohexidecimal(denary):
+    power = 0
+
+n = int(input("Please enter an integer:"))
---
+++ hexidecimal_to_denary.py
@@ -0,0 +1,17 @@
+def hextodenary(hex):
+    value = 0
+    power = 0
+    for i in reversed(hex):
+        if '0' <= i <= '9':
+            digit_value = int(i)
+        elif 'A' <= i <= 'F':
+            digit_value = 10 + (ord(i)-ord('A'))
+        else:
+            return "cannot convert"
+        
+        value = digit_value * (16 ** power)
+        power += 1
+    return value
+
+h = str(input("Please enter a hex: "))
+print(hextodenary(h))
