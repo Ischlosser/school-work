cafc9ca1744ba4165fcf6bdcac464df0d0fc15c0
IschlosserLaptop <200~205675049+Ischlosser@users.noreply.github.com>
2025-09-22 09:26:28 +0200

    added half of validation

--- binary_to_denary.py
+++ binary_to_denary.py
@@ -1,6 +1,9 @@
 def binarytodenary(binary):
     power = 0
     value = 0
+    if len(binary) != 8:
+        return "Please enter exactly 8 bits"
+ 
     for i in range(len(binary) -1, -1, -1):
         if binary[i] == '1':
            value += 2 ** power
