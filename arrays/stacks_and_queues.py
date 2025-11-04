25adb6c26d66ab15f36bfc06c894dc15a127089e
IschlosserLaptop <200~205675049+Ischlosser@users.noreply.github.com>
2025-11-04 21:09:30 +0100

    finished stack and queue assignment

--- arrays/stacks_and_queues.py
+++ arrays/stacks_and_queues.py
@@ -1,9 +1,35 @@
 def stack():
-    subjects = ("", "", "", "", "", "", "", "", "", "")
-    for objects in subjects: #popping on to stack
-        subjects[objects]
-    return
+    subjects = []
+    for i in range(10):
+        subject = input(f"Enter subject #{i+1} (max. 10, enter 'finished' to stop): ")
+        if subject.lower() == 'finished':
+            break
+        subjects.append(subject)
+        print(subjects)
+    while subjects:
+        pop = subjects.pop()
+        print(pop)
+    return subjects
 
 def queue():
-    subjects = ()
-    return
+    subjects = []
+    for i in range(10):
+        subject = input(f"Enter subject #{i+1} (max. 10, enter 'finished' to stop): ")
+        if subject.lower() == 'finished':
+            break
+        subjects.append(subject)
+        print(subjects)
+    while subjects:
+        dequeued_subject = subjects.pop(0)
+        print(dequeued_subject)
+    return subjects
+
+while True:
+    a = 0
+    a = int(input("Would you like to queue(0) or stack(1) your subjects? "))
+    if a == 0:
+        queue()
+    elif a == 1:
+        stack()
+    else:
+        print("Bad input, try again")
