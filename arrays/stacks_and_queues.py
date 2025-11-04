794d7e907b20cf9bfa37d1070f61a2f08a30ddcb
IschlosserLaptop <200~205675049+Ischlosser@users.noreply.github.com>
2025-11-04 13:01:35 +0100

    stack assignment added

---
+++ arrays/stacks_and_queues.py
@@ -0,0 +1,9 @@
+def stack():
+    subjects = ("", "", "", "", "", "", "", "", "", "")
+    for objects in subjects: #popping on to stack
+        subjects[objects]
+    return
+
+def queue():
+    subjects = ()
+    return
