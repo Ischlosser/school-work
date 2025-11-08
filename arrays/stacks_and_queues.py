9d5333330484d9c5aeb31e8674aeed275ebde417
IschlosserLaptop <200~205675049+Ischlosser@users.noreply.github.com>
2025-11-08 23:23:54 +0100

    stack and queues finished

--- arrays/stacks_and_queues.py
+++ arrays/stacks_and_queues.py
@@ -1,35 +1,62 @@
-def stack():
-    subjects = []
-    for i in range(10):
-        subject = input(f"Enter subject #{i+1} (max. 10, enter 'finished' to stop): ")
-        if subject.lower() == 'finished':
-            break
-        subjects.append(subject)
-        print(subjects)
-    while subjects:
-        pop = subjects.pop()
-        print(pop)
-    return subjects
+stack_subjects = []
+queue_subjects = []
+def push():
+    obj = input("Please enter element: ")
+    stack_subjects.append(obj)
+    if len(stack_subjects) > 10:
+        stack_subjects.pop()
+        print(stack_subjects)
+        return "Stack Overflow"
+    return stack_subjects
 
-def queue():
-    subjects = []
-    for i in range(10):
-        subject = input(f"Enter subject #{i+1} (max. 10, enter 'finished' to stop): ")
-        if subject.lower() == 'finished':
-            break
-        subjects.append(subject)
-        print(subjects)
-    while subjects:
-        dequeued_subject = subjects.pop(0)
-        print(dequeued_subject)
-    return subjects
+def pop():
+    if not stack_subjects:
+        return "Stack Underflow"
+    else:
+        popped = stack_subjects.pop()
+        print("Subject popped: " + str(popped))
+        return stack_subjects
+
+def peek():
+    return stack_subjects[-1]
+
+
+def enqueue():
+    obj = input("Please enter element: ")
+    queue_subjects.append(obj)
+    if len(queue_subjects) > 10:
+        queue_subjects.pop()
+        return "Queue Overflow"
+    return queue_subjects
+
+def dequeue():
+    if not queue_subjects:
+        return "Queue Underflow"
+    else:
+        dequeued = queue_subjects.pop(0)
+        print("Subject dequeued: " + str(dequeued))
+        return queue_subjects
 
 while True:
-    a = 0
-    a = int(input("Would you like to queue(0) or stack(1) your subjects? "))
-    if a == 0:
-        queue()
-    elif a == 1:
-        stack()
+    print("####################")
+    print("Would you like to: ")
+    print("1: Push")
+    print("2: Pop")
+    print("3: Peek")
+    print("4: Enqueue")
+    print("5: Dequeue")
+    a = input("Enter: ")
+    print("####################")
+    print()
+    if int(a) == 1:
+        print(push())
+    elif int(a) == 2:
+        print(pop())
+    elif int(a) == 3:
+        print(peek())
+    elif int(a) == 4:
+        print(enqueue())
+    elif int(a) == 5:
+        print(dequeue())
     else:
-        print("Bad input, try again")
+        print("Invalid Input")
