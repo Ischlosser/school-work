515e67fb8d302b15e2a7e36f75149469872a1555
Ischlosser <205675049+Ischlosser@users.noreply.github.com>
2025-11-13 20:22:58 +0100

    die rolling csv extension added

--- probability/die_rolling.py
+++ probability/die_rolling.py
@@ -1,19 +1,29 @@
-import random
+import random, csv
+
 dies = int(input("How many dies would you like to roll? "))
-rolls = int(input("How many times would you like to roll?"))
+rolls = int(input("How many times would you like to roll? "))
 
 def rolling(dies, rolls):
     roll = []
-    results = [[] for _ in range(6*dies)]
-    for i in range(rolls):
+    final = [[f"{q}"] for q in range(6*dies+2)]
+    results = [[] for _ in range(6*dies+2)]
+    for k in range(rolls):
         for j in range(dies):
             roll.append(random.randint(1, 6))
-        print(roll)
-        print(sum(roll))
+        #print(roll)
+        #print(sum(roll))
         results[sum(roll)+1].append(sum(roll))
         roll = []
-    for i in range(6*dies): #fix this 
-        print(f"The amount of {i-1} is {len(results[i])}")
+    for i in range(dies ,6*dies+1): 
+        final[i].append(len(results[i+1]))
+        print(f"The amount of {i} is {len(results[i+1])}")
+    #print(results)
+    with open("die_rolling_stats.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["Sum", "Frequency"])
+        for i in range(dies, 6 * dies+1):
+            writer.writerow(final[i])
+        writer.writerow(["Rolls", rolls])
 
 
 rolling(dies, rolls)
