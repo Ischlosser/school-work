e1599ee51672edf3fe8d6851951d9f79a668765e
IschlosserLaptop <200~205675049+Ischlosser@users.noreply.github.com>
2025-11-15 13:37:37 +0100

    tree traversal coded, die rolling ploting started

--- probability/die_rolling.py
+++ probability/die_rolling.py
@@ -1,4 +1,5 @@
 import random, csv
+import matplotlib.pyplot as mpl
 
 dies = int(input("How many dies would you like to roll? "))
 rolls = int(input("How many times would you like to roll? "))
@@ -14,16 +15,25 @@ def rolling(dies, rolls):
         #print(sum(roll))
         results[sum(roll)+1].append(sum(roll))
         roll = []
-    for i in range(dies ,6*dies+1): 
+    for i in range(dies, 6*dies+1): 
         final[i].append(len(results[i+1]))
         print(f"The amount of {i} is {len(results[i+1])}")
     #print(results)
     with open("die_rolling_stats.csv", "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(["Sum", "Frequency"])
-        for i in range(dies, 6 * dies+1):
-            writer.writerow(final[i])
+        for l in range(dies, 6 * dies+1):
+            writer.writerow(final[l])
         writer.writerow(["Rolls", rolls])
+    mpl.title(f'Frequency of Sums from Rolling {dies} Dice ({rolls} Rolls)')
+    mpl.xlabel('Sum of Dice')
+    mpl.ylabel('Frequency')
+    for x in range(dies, 6*dies+1):
+        mpl.plot(final[i][0], final[i][1], marker='o', linestyle='-', color='b')
+    mpl.xticks([p for p in range(6*dies+2)]) #needs fix
+    mpl.show()
+
+
 
 
 rolling(dies, rolls)
--- trees/tree_traversal.py
+++ trees/tree_traversal.py
@@ -1,1 +1,59 @@
-names = {"Imre":[]}
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+imre = TreeNode('Imre')
+farid = TreeNode('Farid')
+lekai = TreeNode('Lekai')
+jaymes = TreeNode('Jaymes')
+vadim = TreeNode('Vadim')
+jacob = TreeNode('Jacob')
+raphael = TreeNode('Raphael')
+
+imre.left = farid
+imre.right = lekai
+
+lekai.left = jaymes
+lekai.right = vadim
+
+jaymes.left = jacob
+
+vadim.left = raphael
+
+def inOrderTrav(node):
+    if node:
+        inOrderTrav(node.left)
+        print(node.data)
+        inOrderTrav(node.right)
+        
+def preOrderTrav(node):
+    if node:
+        print(node.data)
+        preOrderTrav(node.left)
+        preOrderTrav(node.right)
+
+def postOrderTrav(node):
+    if node:
+        postOrderTrav(node.left)
+        postOrderTrav(node.right)
+        print(node.data)
+
+while True:
+    print("#########################################")
+    print("How would you like to traverse the tree:")
+    print("1: In order")
+    print("2: Pre order")
+    print("3: Post order")
+    a = input("Enter: ")
+    if int(a) == 1:
+        inOrderTrav(imre)
+    elif int(a) == 2:
+        preOrderTrav(imre)
+    elif int(a) == 3:
+        postOrderTrav(imre)
+    elif int(a) == 4:
+        break
+    else:
+        print("Error try again")
